f41e690dddde5987bc4d70cbeef9067b6fd7b279
inesrollan <inesrmh@gmail.com>
2020-04-07 13:52:16 +0200

    7 abr

--- repositorios/src/main/java/es/avalon/repositorios/jpa/LibroRepositoryJPA.java
+++ repositorios/src/main/java/es/avalon/repositorios/jpa/LibroRepositoryJPA.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
@@ -12,46 +13,67 @@ import es.avalon.repositorios.LibroRepository;
 
 public class LibroRepositoryJPA implements LibroRepository {
 
+	EntityManagerFactory emf;
+	EntityManager em;
+
+	public LibroRepositoryJPA() {
+
+		emf = Persistence.createEntityManagerFactory("UnidadBiblioteca");
+		em = emf.createEntityManager();
+	}
+
 	@Override
 	public List<Libro> buscarTodos() {
 
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("UnidadBiblioteca");
-		EntityManager em = emf.createEntityManager();
 		TypedQuery<Libro> consulta = em.createQuery("select l from Libro l", Libro.class);
 
-		List<Libro> lista = consulta.getResultList();
-
-		return lista;
+		return consulta.getResultList();
 	}
 
 	@Override
 	public Libro buscarPorISBN(String isbn) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return em.find(Libro.class, isbn);
 	}
 
 	@Override
 	public Libro buscarPorTitulo(String titulo) {
-		// TODO Auto-generated method stub
-		return null;
+
+		TypedQuery<Libro> consulta = em.createQuery("select l from Libro l where l.titulo=:titulo", Libro.class);
+		consulta.setParameter("titulo", titulo);
+
+		return consulta.getSingleResult();
 	}
 
 	@Override
 	public void insertar(Libro libro) {
-		// TODO Auto-generated method stub
+
+		EntityTransaction t = em.getTransaction();
+		t.begin();
+		em.persist(libro);
+		t.commit();
 
 	}
 
 	@Override
 	public void salvar(Libro libro) {
-		// TODO Auto-generated method stub
+
+		EntityTransaction t = em.getTransaction();
+		t.begin();
+		em.merge(libro);
+		t.commit();
 
 	}
 
 	@Override
 	public void borrar(Libro libro) {
-		// TODO Auto-generated method stub
 
+		EntityTransaction t = em.getTransaction();
+		t.begin();
+		// Libro libroBorrar)em.merge(libro);
+		Libro libroBorrar = em.find(Libro.class, libro.getIsbn());
+		em.remove(libroBorrar);
+		t.commit();
 	}
 
 }
--- repositorios/src/test/java/es/avalon/repositorios/jpa/test/LibroRepositoryJPATest.java
+++ repositorios/src/test/java/es/avalon/repositorios/jpa/test/LibroRepositoryJPATest.java
@@ -2,16 +2,16 @@ package es.avalon.repositorios.jpa.test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import es.avalon.dominio.Libro;
 import es.avalon.repositorios.LibroRepository;
 import es.avalon.repositorios.jpa.LibroRepositoryJPA;
 
 public class LibroRepositoryJPATest {
 
-	
-	
 	public LibroRepositoryJPATest() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -19,11 +19,43 @@ public class LibroRepositoryJPATest {
 
 	@Test
 	public void test_buscar_todos() {
+
+		LibroRepository mirepositorio = new LibroRepositoryJPA();
+		assertThat(mirepositorio.buscarTodos().size(), greaterThanOrEqualTo(4));
+
+	}
+
+	@Test
+	public void test_buscar_por_ISBN() {
+
+		LibroRepository mirepositorio = new LibroRepositoryJPA();
+
+		Libro libro = mirepositorio.buscarPorISBN("1AB");
+
+		assertEquals("1AB", libro.getIsbn());
+		assertEquals("Java", libro.getTitulo());
+		assertEquals("cecilio", libro.getAutor());
+	}
+
+	@Test
+	public void test_buscar_por_Titulo() {
+
+		LibroRepository mirepositorio = new LibroRepositoryJPA();
+		
+		Libro libro = mirepositorio.buscarPorTitulo("Java");
+
+		assertEquals("Java", libro.getTitulo());
+		assertEquals("1AB", libro.getIsbn());
+		assertEquals("cecilio", libro.getAutor());
+	}
 	
-		LibroRepository mirepositorio= new LibroRepositoryJPA();	
-		assertThat(mirepositorio.buscarTodos().size(),greaterThanOrEqualTo(4));
+	@Test
+	public void test_borrar_libro() {
 		
+		LibroRepository mirepositorio = new LibroRepositoryJPA();
 		
+		Libro libro=new Libro("1AB");
+		mirepositorio.borrar(libro);
 		
 	}
 
