b6a8acf2639fa0ddad53047c6fa914c8d3d7cbfa
Razvan Pascal <razvanpascal@outlook.com>
2020-04-07 12:14:55 +0200

    repositorio JPA

--- repositorios/src/main/java/es/avalon/repositorios/jpa/LibroRepositoryJPA.java
+++ repositorios/src/main/java/es/avalon/repositorios/jpa/LibroRepositoryJPA.java
@@ -26,14 +26,26 @@ public class LibroRepositoryJPA implements LibroRepository {
 
 	@Override
 	public Libro buscarPorISBN(String isbn) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("UnidadBiblioteca");
+		EntityManager em = emf.createEntityManager();
+		
+		Libro libro = em.find(Libro.class, isbn);
+		
+		
+		return libro;
 	}
 
 	@Override
 	public Libro buscarPorTitulo(String titulo) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("UnidadBiblioteca");
+		EntityManager em = emf.createEntityManager();
+		
+		TypedQuery<Libro> consulta = em.createQuery("select l from Libro l where l.titulo=:titulo", Libro.class);
+		consulta.setParameter("titulo", titulo);
+		Libro libro = consulta.getSingleResult();
+
+		return libro;
 	}
 
 	@Override
--- repositorios/src/test/java/es/avalon/repositorios/jpa/test/LibroRepositoryJPATest.java
+++ repositorios/src/test/java/es/avalon/repositorios/jpa/test/LibroRepositoryJPATest.java
@@ -2,9 +2,12 @@ package es.avalon.repositorios.jpa.test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
+import es.avalon.dominio.Libro;
 import es.avalon.repositorios.LibroRepository;
 import es.avalon.repositorios.jpa.LibroRepositoryJPA;
 
@@ -23,8 +26,29 @@ public class LibroRepositoryJPATest {
 		LibroRepository mirepositorio= new LibroRepositoryJPA();	
 		assertThat(mirepositorio.buscarTodos().size(),greaterThanOrEqualTo(4));
 		
-		
-		
 	}
+	
+	@Test
+	public void test_buscar_por_ISBN() {
+		LibroRepository mirepositorio= new LibroRepositoryJPA();
+		Libro libro = mirepositorio.buscarPorISBN("1AB");
+		assertEquals("1AB", libro.getIsbn());
+		assertEquals("Java", libro.getTitulo());
+		assertEquals("cecilio", libro.getAutor());
+		assertEquals(10, libro.getPrecio());
 
+	}
+	
+	@Test
+	public void test_buscar_por_titulo() {
+		LibroRepository mirepositorio= new LibroRepositoryJPA();
+		Libro libro = mirepositorio.buscarPorTitulo("html");
+		assertEquals("3BC", libro.getIsbn());
+		assertEquals("html", libro.getTitulo());
+		assertEquals("gema", libro.getAutor());
+		assertEquals(8, libro.getPrecio());
+		
+
+	}
+	
 }
