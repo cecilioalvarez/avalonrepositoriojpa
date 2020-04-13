e4e117aab9aba6c099178182f8e6f5fe2c60f90b
Formacion <Formacion@5CD9494ZLKC>
2020-04-13 13:59:10 +0200

    Cambio variables emf

--- repositorios/src/main/java/es/avalon/repositorios/jpa/CategoriaRepositoryJPA.java
+++ repositorios/src/main/java/es/avalon/repositorios/jpa/CategoriaRepositoryJPA.java
@@ -1,37 +1,34 @@
 package es.avalon.repositorios.jpa;
 
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
-
 import es.avalon.dominio.Categoria;
-import es.avalon.dominio.Libro;
 import es.avalon.repositorios.CategoriaRepository;
 
 public class CategoriaRepositoryJPA implements CategoriaRepository{
 
 
-	EntityManagerFactory emf;
-	EntityManager em;
+	EntityManagerFactory entityManagerFactory;
+	EntityManager entityManager;
 
 	public CategoriaRepositoryJPA() {
 
-		emf = Persistence.createEntityManagerFactory("UnidadBiblioteca");
-		em=emf.createEntityManager();
+		entityManagerFactory = Persistence.createEntityManagerFactory("UnidadBiblioteca");
+		entityManager=entityManagerFactory.createEntityManager();
 	}
 	
 	public Categoria buscarPorNombre(String nombre) {
 		
-		return em.find(Categoria.class, nombre);
+		return entityManager.find(Categoria.class, nombre);
 	}
 	
 	
 	public List<Categoria> buscarTodos() {
 
-		TypedQuery<Categoria> consulta = em.createQuery("select c from Categoria c", Categoria.class);
+		TypedQuery<Categoria> consulta = entityManager.createQuery("select c from Categoria c", Categoria.class);
 		return consulta.getResultList();		
 	}
 
--- repositorios/src/main/java/es/avalon/repositorios/jpa/LibroRepositoryJPA.java
+++ repositorios/src/main/java/es/avalon/repositorios/jpa/LibroRepositoryJPA.java
@@ -7,68 +7,64 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
-
 import es.avalon.dominio.Libro;
 import es.avalon.repositorios.LibroRepository;
 
 public class LibroRepositoryJPA implements LibroRepository {
 
-	EntityManagerFactory emf;
-	EntityManager em;
+	EntityManagerFactory entityManagerFactory;
+	EntityManager entityManager;
 
 	public LibroRepositoryJPA() {
 
-		emf = Persistence.createEntityManagerFactory("UnidadBiblioteca");
-		em=emf.createEntityManager();
+		entityManagerFactory = Persistence.createEntityManagerFactory("UnidadBiblioteca");
+		entityManager = entityManagerFactory.createEntityManager();
 	}
-	
+
 	public List<Libro> buscarTodos() {
 
-		TypedQuery<Libro> consulta = em.createQuery("select l from Libro l", Libro.class);
-		return consulta.getResultList();		
+		TypedQuery<Libro> consulta = entityManager.createQuery("select l from Libro l", Libro.class);
+		return consulta.getResultList();
 	}
 
 	public Libro buscarPorISBN(String isbn) {
-		 return em.find(Libro.class, isbn);
+		return entityManager.find(Libro.class, isbn);
 	}
-	
+
 	public Libro buscarPorTitulo(String titulo) {
 
-		TypedQuery<Libro> consulta = em.createQuery("select l from Libro l where l.titulo=:titulo", Libro.class);
-		consulta.setParameter("titulo", titulo);
-		return consulta.getSingleResult();
+		return entityManager.find(Libro.class, titulo);
 	}
-	
+
 	public void insertar(Libro libro) {
-		EntityTransaction et = em.getTransaction();
-		et.begin();
-		em.persist(libro);
-		et.commit();
-	}
-	
-	public void salvar(Libro libro) {
 		
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		entityManager.persist(libro);
+		entityTransaction.commit();
+	}
+
+	public void salvar(Libro libro) {
 		try {
-			EntityTransaction et = em.getTransaction();
-			et.begin();
-			em.merge(libro);
-			et.commit();
+			EntityTransaction entityTransaction = entityManager.getTransaction();
+			entityTransaction.begin();
+			entityManager.merge(libro);
+			entityTransaction.commit();
 		} catch (Exception e) {
-			
-			if(em!=null) {
-				em.close();
+
+			if (entityManager != null) {
+				entityManager.close();
 			}
 		}
-
 	}
-	
+
 	public void borrar(Libro libro) {
 
-		EntityTransaction et = em.getTransaction();
-		et.begin();
-		Libro libroBorrar = em.merge(libro);
-		em.remove(libroBorrar);
-		et.commit();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		Libro libroBorrar = entityManager.merge(libro);
+		entityManager.remove(libroBorrar);
+		entityTransaction.commit();
 	}
 
 }
