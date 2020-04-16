02d56dca07bb37809ccf013678217a0207ec43dd
Formacion <Formacion@5CD9494ZLTC>
2020-04-16 12:04:32 +0200

    resuelto

--- repositorios/src/main/java/es/avalon/repositorios/jpa/LibroRepositoryJPA.java
+++ repositorios/src/main/java/es/avalon/repositorios/jpa/LibroRepositoryJPA.java
@@ -3,15 +3,16 @@ package es.avalon.repositorios.jpa;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
 
 import es.avalon.dominio.Libro;
 import es.avalon.repositorios.LibroRepository;
 
+@Repository
 public class LibroRepositoryJPA implements LibroRepository {
 	
 	@PersistenceContext
