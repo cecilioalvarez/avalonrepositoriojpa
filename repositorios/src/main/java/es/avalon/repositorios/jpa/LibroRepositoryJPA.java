1b335e03ac51ebfc38be69752bb7a688e893ac4d
cursos <cursos@localhost>
2020-04-16 10:50:21 +0200

    ultimos cambios

--- repositorios/src/main/java/es/avalon/repositorios/jpa/LibroRepositoryJPA.java
+++ repositorios/src/main/java/es/avalon/repositorios/jpa/LibroRepositoryJPA.java
@@ -14,6 +14,7 @@ import es.avalon.repositorios.LibroRepository;
 public class LibroRepositoryJPA implements LibroRepository {
 
 	EntityManagerFactory emf;
+	// dependemos de un entitymanager
 	EntityManager em;
 
 	public LibroRepositoryJPA() {
